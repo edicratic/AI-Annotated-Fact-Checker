7b851e9af20cf295a7d078db8393685d3f758097
yuktmitash <ymitash3@gatech.edu>
2020-04-01 10:07:39 -0400

    improved

--- expandLibrary.js
+++ expandLibrary.js
@@ -3,6 +3,7 @@ TOOL_TIP_CLASSNAME = 'edicratic-add-library-tooltip'
 TOOL_TIP_TEXT_CLASSNAME = 'tooltiptext';
 CHECK_CLASS_NAME = 'fa fa-check fa-2x edicratic-yes';
 X_CLASS_NAME = 'fa fa-times fa-2x edicratic-no';
+
 function analyzeTextForSending() {
     const range = window.getSelection().getRangeAt(0);
     let text = window.getSelection().toString();
@@ -20,34 +21,16 @@ function analyzeTextForSending() {
     tooltip.setAttribute('data-content', text);
     tooltip.style.width = `${span.clientWidth}px`
     tooltip.style.top = `${window.pageYOffset + span.getBoundingClientRect().top -span.clientHeight - 10}px`;
-    tooltip.style.left = `${span.getBoundingClientRect().left - span.clientWidth}px`
+    tooltip.style.left = `${span.getBoundingClientRect().left - text.length}px`
     document.body.prepend(tooltip);
-    let cross = document.getElementsByClassName(X_CLASS_NAME)[0];
-    let check = document.getElementsByClassName(CHECK_CLASS_NAME)[0];
-    cross.onclick = () => replaceElement(cross, cross.innerText || cross.textContent);
-    check.onclick = () => replaceElement(check, check.innerText || check.textContent);
 }
 
 function removeHighlightedSpans() {
     remove(document.getElementsByClassName(TOOL_TIP_CLASSNAME));
-    let spans = document.getElementsByClassName(EDICRATIC_HIGHLIGHTED_TEXT_CLASS);
-    for (var i = 0; i < spans.length; i++) {
-        let el = spans[i];
-        let str = el.textContent || el.innerText;
-        replaceElement(el, str);
-    }
 }
 
 function remove(collection) {
     for (var i = 0; i < collection.length; i++) {
         collection[i].parentNode.removeChild(collection[i]);
     }
-}
-
-function replaceElement(element, text) {
-    var tmpObj=document.createElement("div");
-    tmpObj.innerHTML='<!--THIS DATA SHOULD BE REPLACED-->';
-    ObjParent = element.parentNode;
-    ObjParent.replaceChild(tmpObj, element); 
-    ObjParent.innerHTML = ObjParent.innerHTML.replace('<div><!--THIS DATA SHOULD BE REPLACED--></div>', text);
 }
