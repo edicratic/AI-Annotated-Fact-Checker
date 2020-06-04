daf9ea31142772680140c3e3e9a1e1203d99b4e6
Christopher Scherban <christopher.scherban@gmail.com>
2020-06-03 23:40:53 -0400

    switched auth to webcheck page

--- background.js
+++ background.js
@@ -177,80 +177,8 @@ function getToken(id_token){
 }
 
 function oauthFlow(){
-  var manifest = chrome.runtime.getManifest();
-  var clientId = encodeURIComponent(manifest.oauth2.client_id);
-  var scopes = encodeURIComponent(manifest.oauth2.scopes.join(' '));
-  var redirectUri = encodeURIComponent('urn:ietf:wg:oauth:2.0:oob:auto');
-  var url = 'https://accounts.google.com/o/oauth2/auth' +
-            '?client_id=' + clientId +
-            '&response_type=id_token' +
-            '&access_type=offline' +
-            '&redirect_uri=' + redirectUri +
-            '&scope=' + scopes;
-
-  var RESULT_PREFIX = ['Success', 'Denied', 'Error'];
   chrome.tabs.create({'url': 'about:blank'}, function(authenticationTab) {
-      chrome.tabs.onUpdated.addListener(function googleAuthorizationHook(tabId, changeInfo, tab) {
-          if (tabId === authenticationTab.id) {
-              // console.log(tab.title);
-			  if (tab.title === null || tab.title === undefined){
-				  return;
-			  }
-              var titleParts = tab.title.split(' ', 2);
-
-              var result = titleParts[0];
-              if (titleParts.length == 2 && RESULT_PREFIX.indexOf(result) >= 0) {
-                  chrome.tabs.onUpdated.removeListener(googleAuthorizationHook);
-                  chrome.tabs.remove(tabId);
-
-                  var response = titleParts[1];
-                  // console.log("hey");
-                  const urlParams = new URLSearchParams(response);
-                  let id_token = null;
-                  switch (result) {
-                      case 'Success':
-                          // console.log("SUCCESSS")
-                          // console.log(response);
-                          id_token = urlParams.get("id_token");
-                          getToken(id_token).then(res => {
-                            chrome.storage.local.set({'authStatus': "Authenticated"});
-                            createDefaultBlackList()
-                          }).catch(err => {
-                              chrome.storage.local.set({'authStatus': "Logged Out"});
-                              console.log(err);
-                          });
-                      break;
-                      case 'Denied':
-                          // console.log("DENIEDDD");
-                          chrome.storage.local.set({'authStatus': "Logged Out"});
-                          // Example: error_subtype=access_denied&error=immediate_failed
-                          // console.log(response);
-                      break;
-                      case 'Error':
-                          //console.log("ERRRORRROROOR");
-                          // console.log(response);
-                          id_token = urlParams.get("id_token");
-                          if (id_token == null || id_token == undefined){
-                              //console.log("an actual error")
-                              //TODO Handle / log this somehow
-                              chrome.storage.local.set({'authStatus': "Logged Out"});
-                          }else{
-                              getToken(id_token).then(res => {
-                                  chrome.storage.local.set({'authStatus': "Authenticated"});
-                                  createDefaultBlackList();
-                                  // console.log(res);
-                              }).catch(err => {
-                                  chrome.storage.local.set({'authStatus': "Logged Out"});
-                                  console.log(err);
-                              });
-                          }
-                      break;
-                  }
-              }
-          }
-      });
-
-      chrome.tabs.update(authenticationTab.id, {'url': url});
+      chrome.tabs.update(authenticationTab.id, {'url': 'https://webcheck.edicratic.com/login.html'});
   });
 };
 
